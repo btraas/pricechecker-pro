8e9ab58b5f0b7cfe5351b723754482b0d39cb3f1
Brayden Traas <btraas@gmail.com>
2017-03-12 14:09:42 -0700

    remove defines from git

--- www/defines.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DOMAIN', 'pricechecker.pro:444');
-define('SITENAME', 'test.pricechecker.pro');
-define('PROJECT_ROOT', '/var/www/test.pricechecker-pro/www');
-chdir(PROJECT_ROOT."/");
-
-?>
