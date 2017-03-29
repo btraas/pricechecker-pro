98cb57e0a161584ccc7cb580468bbead3161c972
liang611zh <zhaoliangbz@gmail.com>
2017-03-29 11:05:18 -0700

    add share link to index page

--- www/inc/head.php
+++ www/inc/head.php
@@ -40,7 +40,30 @@
 	<link rel="stylesheet" href="css/mtl-styles.css">
     <link rel="stylesheet" href="css/styles.css">
     <style>
-    #view-source {
+        ul.share-buttons{
+            list-style: none;
+            padding: 0;
+        }
+        ul.share-buttons li{
+            display: inline;
+        }
+        ul.share-buttons .sr-only {
+            position: absolute;
+            clip: rect(1px 1px 1px 1px);
+            clip: rect(1px, 1px, 1px, 1px);
+            padding: 0;
+            border: 0;
+            height: 1px;
+            width: 1px;
+            overflow: hidden;
+        }
+        ul.share-buttons img{
+            width: 32px;
+        }
+        a{text-decoration: none !important;}
+
+
+        #view-source {
       position: fixed;
       display: block;
       right: 0;
--- www/inc/lookup/index.php
+++ www/inc/lookup/index.php
@@ -3,5 +3,28 @@
 <?php include('inc/lookup/upc-input.php'); ?>
             
 <!-- Add spacer to push Footer down when not enough content -->
-<div class="mdl-layout-spacer" style='margin-bottom: 56px'></div>
+<div class="mdl-layout-spacer" style='margin-bottom: 56px'>
+    <ul class="share-buttons">
+        <li>
+            <a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fpricechecker.pro%3A444%2F&t=" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL) + '&t=' + encodeURIComponent(document.URL)); return false;">
+                <img alt="Share on Facebook" src="images/social_flat_rounded_rects_svg/Facebook.svg">
+            </a>
+        </li>
+        <li>
+            <a href="https://twitter.com/intent/tweet?source=http%3A%2F%2Fpricechecker.pro%3A444%2F&text=:%20http%3A%2F%2Fpricechecker.pro%3A444%2F" target="_blank" title="Tweet" onclick="window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.title) + ':%20'  + encodeURIComponent(document.URL)); return false;">
+                <img alt="Tweet" src="images/social_flat_rounded_rects_svg/Twitter.svg">
+            </a>
+        </li>
+        <li>
+            <a href="https://plus.google.com/share?url=http%3A%2F%2Fpricechecker.pro%3A444%2F" target="_blank" title="Share on Google+" onclick="window.open('https://plus.google.com/share?url=' + encodeURIComponent(document.URL)); return false;">
+                <img alt="Share on Google+" src="images/social_flat_rounded_rects_svg/Google+.svg">
+            </a>
+        </li>
+        <li>
+            <a href="mailto:?subject=&body=:%20http%3A%2F%2Fpricechecker.pro%3A444%2F" target="_blank" title="Send email" onclick="window.open('mailto:?subject=' + encodeURIComponent(document.title) + '&body=' +  encodeURIComponent(document.URL)); return false;">
+                <img alt="Send email" src="images/social_flat_rounded_rects_svg/Email.svg">
+            </a>
+        </li>
+    </ul>
+</div>
 <?php include('inc/footer.php'); ?>
