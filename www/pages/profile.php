0b70679efd55c7f0e86422110bc2f4a9d2bc1b14
pkao <paul_kao@hotmail.ca>
2017-03-29 11:59:36 -0700

    Created a simple profile page

---
+++ www/pages/profile.php
@@ -0,0 +1,42 @@
+<?php
+
+//include('inc\head.php');
+?>
+<style>
+    #profilepic{
+        border: 2px solid black;
+        padding: 10px;
+        max-width: 110px;
+        display: inline-block;
+    }
+    
+    #about{
+        border: 2px solid black;
+        display: inline-block;
+        padding: 10px;
+    }
+
+</style>
+<div>
+    <div id="profilepic">
+        <div>
+            <img src="http://www.westsalemtitansbaseball.com/uploads/3/8/4/0/38404961/505107.png" alt="pic" height="100px" width="100px">
+        </div>
+        <div>
+            <p>A description</p>
+        </div>
+    </div>
+    <div id="about">
+        <div>
+            <p>About Me:</p>
+            <ul>
+                <li>Name:</li>
+                <li>Age:</li>
+                <li>Date of Birth:</li>
+                <li>Occupation:</li>
+                <li>Interests:</li>
+                <li>Currently Following:</li>
+            </ul>
+        </div>
+    </div>
+</div>
