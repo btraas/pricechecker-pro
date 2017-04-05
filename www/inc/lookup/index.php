60e3697767594d55832193ad783e9e20a8626c0a
liang611zh <zhaoliangbz@gmail.com>
2017-04-05 12:02:22 -0700

    testing for the email share

--- www/inc/lookup/index.php
+++ www/inc/lookup/index.php
@@ -11,7 +11,7 @@
         <li><a href="http://www.tumblr.com/share?v=3&u=&t=&s=" target="_blank" title="Post to Tumblr" onclick="window.open('http://www.tumblr.com/share?v=3&u=' + encodeURIComponent(document.URL) + '&t=' +  encodeURIComponent(document.title)); return false;"><img alt="Post to Tumblr" src="images/color/Tumblr.png"></a></li>
         <li><a href="http://pinterest.com/pin/create/button/?url=&description=" target="_blank" title="Pin it" onclick="window.open('http://pinterest.com/pin/create/button/?url=' + encodeURIComponent(document.URL) + '&description=' +  encodeURIComponent(document.title)); return false;"><img alt="Pin it" src="images/color/Pinterest.png"></a></li>
         <li><a href="http://www.reddit.com/submit?url=&title=" target="_blank" title="Submit to Reddit" onclick="window.open('http://www.reddit.com/submit?url=' + encodeURIComponent(document.URL) + '&title=' +  encodeURIComponent(document.title)); return false;"><img alt="Submit to Reddit" src="images/color/Reddit.png"></a></li>
-        <li><a href="mailto:?subject=&body=:%20" target="_blank" title="Send email" onclick="window.open('mailto:?subject=' + encodeURIComponent(document.title) + '&body=' +  encodeURIComponent(document.URL)); return false;"><img alt="Send email" src="images/color/Email.png"></a></li>
+        <li><a href="mailto:?subject=I wanted you to see this site&amp;body=Check out this site https://pricechecker.pro:444/" target="_blank" title="Send email" onclick="window.open('mailto:?subject=' + encodeURIComponent(document.title) + '&body=' +  encodeURIComponent(document.URL)); return false;"><img alt="Send email" src="images/color/Email.png"></a></li>
     </ul>
 </div>
 <?php include('inc/footer.php'); ?>
